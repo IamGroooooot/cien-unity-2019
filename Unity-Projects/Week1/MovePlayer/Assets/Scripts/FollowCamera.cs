e7b69e6d1f6414ff34e0ed84917851888373eb09
IamGroooooot <dury.ko@gmail.com>
2019-11-02 21:57:48 +0900

    Add Week1 Unity File

---
+++ Unity-Projects/Week1/MovePlayer/Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    public Vector3 offset;
+    public GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = target.transform.position + offset;
+    }
+}
---
+++ Unity-Projects/Week1/MovePlayer/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 상
+        if (Input.GetKey(KeyCode.W))
+        {
+            target.transform.Translate(0, 0, 100 * Time.deltaTime);
+
+        }
+        // 하
+        if (Input.GetKey(KeyCode.S))
+        {
+            target.transform.Translate(0, 0, -100 * Time.deltaTime);
+        }
+        // 좌
+        if (Input.GetKey(KeyCode.A))
+        {
+            target.transform.Translate(-100 * Time.deltaTime, 0, 0);
+        }
+        // 우
+        if (Input.GetKey(KeyCode.D))
+        {
+            target.transform.Translate(100 * Time.deltaTime, 0, 0);
+        }
+        // 상
+        if (Input.GetKey(KeyCode.Q))
+        {
+            target.transform.Translate(0, 100 * Time.deltaTime, 0);
+        }
+        // 하
+        if (Input.GetKey(KeyCode.E))
+        {
+            target.transform.Translate(0, -100 * Time.deltaTime, 0);
+        }
+    }
+}
