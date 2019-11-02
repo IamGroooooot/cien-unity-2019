01b9388d0480ac2bcd6b52bc0cba625e11eba7e2
IamGroooooot <dury.ko@gmail.com>
2019-11-03 02:04:42 +0900

    1주차 2주차 PPT 수정, 2주차 코드 추가

---
+++ Unity-Projects/SimpleGame/Assets/Scripts/Shoot.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public GameObject cube;
+    bool flag = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            flag = !flag;
+        }
+        if (flag)
+            Instantiate(cube);
+    }
+}
---
+++ Unity-Projects/Week2/Assets/Scripts/PositionTest/DebugPosition.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugPosition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("자식의 Global Position: " + transform.position);
+        Debug.Log("자식의 Local  Position: " + transform.localPosition);
+    }
+}
