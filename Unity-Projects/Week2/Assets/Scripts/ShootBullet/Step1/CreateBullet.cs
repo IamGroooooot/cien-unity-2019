c6040634d7872fff2dc5e582b6485104c66d1169
IamGroooooot <dury.ko@gmail.com>
2019-11-13 18:57:32 +0900

    2주차 업로드

--- Unity-Projects/Week2/Assets/Scripts/ShootBullet/Step1/CreateBullet.cs
+++ Unity-Projects/Week2/Assets/Scripts/ShootBullet/Step1/CreateBullet.cs
@@ -11,10 +11,10 @@ public class CreateBullet : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // bullet 생성 1 // 어디에 생성? bullet의 position 사용
-        // Instantiate(bulletPrefab);
+        // bullet 생성 1 // 어떻게 생성? (회전도 크기도) bulletPrefab 그대로 //어디에 생성? bullet의 position 사용 
+        Instantiate(bulletPrefab);
 
-        // bullet 생성 2 // 어디에 생성? 게임오브젝트 ShootPosition의 위치
+        // bullet 생성 2 // 어떻게 생성? bulletPrefab의 rotation //어디에 생성? 게임오브젝트 ShootPosition의 위치
         shootPosition = new Vector3(shootTranform.position.x, shootTranform.position.y, shootTranform.position.z);
         Instantiate(bulletPrefab, shootPosition, bulletPrefab.transform.rotation);
     }
---
+++ Unity-Projects/Week2/Assets/Scripts/ShootBullet/Step2/CreateBullet_GetKey.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateBullet_GetKey : MonoBehaviour
+{
+
+    public GameObject bulletPrefab;
+    public Transform shootTranform;
+    private Vector3 shootPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            shootPosition = new Vector3(shootTranform.position.x, shootTranform.position.y, shootTranform.position.z);
+            Instantiate(bulletPrefab, shootPosition, bulletPrefab.transform.rotation);
+        }
+    }
+}
