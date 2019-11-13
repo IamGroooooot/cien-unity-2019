e8330a7fd90953e6cea0998fd35cf706e17a4c78
IamGroooooot <dury.ko@gmail.com>
2019-11-13 14:33:16 +0900

    reupload Codes

---
+++ Unity-Projects/Week2/Assets/Scripts/FindTarget/FindWithTag.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindWithTag : MonoBehaviour
+{
+    private GameObject myTarget;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myTarget = GameObject.FindGameObjectWithTag("MyTag");
+
+        if (myTarget != null)
+        {
+            Debug.Log("GameObject(" + myTarget.name + ")을 " + "찾았습니다");
+        }
+        else
+        {
+            Debug.Log("해당하는 게임 오브젝트를 찾지 못했습니다");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myTarget.GetComponent<Rigidbody>().AddForce(-2, 0, 0);
+    }
+}
